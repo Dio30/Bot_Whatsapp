1303ee81d0984f79b63aab0a82003fb4e73f0669
DionePadilhaMercadoLivre <dione.padilha@mercadolivre.com>
2024-08-17 14:28:06 -0300

    4º commit

--- whatsapp.py
+++ whatsapp.py
@@ -12,13 +12,13 @@ def bot():
     # Respostas automáticas
     if 'menu' in incoming_msg:
         response = "Nosso menu:\n1. Hambúrguer\n2. Pizza\n3. Salada\n4. Bebidas"
-    elif 1 in incoming_msg:
+    elif '1' in incoming_msg:
         response = "Temos os seguintes hambúrgueres:\n- Cheeseburger\n- Hambúrguer Duplo\n- Hambúrguer de Frango"
-    elif 2 in incoming_msg:
+    elif '2' in incoming_msg:
         response = "Temos os seguintes sabores de pizza:\n- Margherita\n- Pepperoni\n- Quatro Queijos"
-    elif 3 in incoming_msg:
+    elif '3' in incoming_msg:
         response = "Temos as seguintes saladas:\n- Salada Caesar\n- Salada Grega\n- Salada de Frutas"
-    elif 4 in incoming_msg:
+    elif '4' in incoming_msg:
         response = "Temos as seguintes bebidas:\n- Refrigerante\n- Suco\n- Água"
     else:
         response = "Olá! Como posso ajudá-lo? Envie 'menu' para ver nossas opções."
