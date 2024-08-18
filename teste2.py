6c0b7abb8f630f1e749fe80a88711be81fe81a40
DionePadilhaMercadoLivre <dione.padilha@mercadolivre.com>
2024-08-18 14:24:07 -0300

    8º commit

--- teste2.py
+++ teste2.py
@@ -21,21 +21,33 @@ headers = {
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    data = request.get_json()
-    
-    # Verifique se recebemos uma mensagem
-    if 'messages' in data['entry'][0]['changes'][0]['value']:
-        # Dados da mensagem recebida
-        messages = data['entry'][0]['changes'][0]['value']['messages'][0]
-        message_text = messages['text']['body'] if 'text' in messages else None
-        sender_id = messages['from']  # O número do telefone do remetente
+    if request.method == 'GET':
+        verify_token = 'dione1234'
+        mode = request.args.get('hub.mode')
+        token = request.args.get('hub.verify_token')
+        challenge = request.args.get('hub.challenge')
         
-        # Lógica de resposta
-        if message_text:
-            reply_text = f"Você disse: {message_text}"
-            send_message(sender_id, reply_text)
+        if mode == 'subscribe' and token == verify_token:
+            return challenge, 200
+        else:
+            return 'Verificação falhou', 403
     
-    return jsonify({'status': 'success'}), 200
+    if request.method == 'POST':
+        data = request.get_json()
+        
+        # Verifique se recebemos uma mensagem
+        if 'messages' in data['entry'][0]['changes'][0]['value']:
+            # Dados da mensagem recebida
+            messages = data['entry'][0]['changes'][0]['value']['messages'][0]
+            message_text = messages['text']['body'] if 'text' in messages else None
+            sender_id = messages['from']  # O número do telefone do remetente
+            
+            # Lógica de resposta
+            if message_text:
+                reply_text = f"Você disse: {message_text}"
+                send_message(sender_id, reply_text)
+        
+        return jsonify({'status': 'success'}), 200
 
 def send_message(recipient_phone_number, message_text):
     payload = {
